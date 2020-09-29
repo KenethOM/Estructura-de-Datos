eeabd9b416ee2bee01168b5475cf0a6322d02cc3
Luis German Ruelas Luna <lgruelas@hotmail.com>
2020-09-29 10:22:04 -0500

    Added node

---
+++ DS/linked_list.py
@@ -0,0 +1,6 @@
+#! /usr/bin/python3
+from node import Node
+
+
+class LinkedList:
+    pass
---
+++ DS/node.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python3
+
+
+class Node:
+    def __init__(self, value, next_node=None):
+        self._value = value
+        self._next_node = next_node
+
+    def get_value(self):
+        return self._value
+
+    def get_next_node(self):
+        return self._next_node
+
+    def set_next_node(self, new_node):
+        self._next_node = new_node
+
+    def __repr__(self):
+        if self.get_next_node():
+            return "{}->{}".format(self.get_value(), self.get_next_node().get_value())
+        else:
+            return str(self.get_value())
