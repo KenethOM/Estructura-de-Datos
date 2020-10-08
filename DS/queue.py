38021e7a1997a613455bc1dd4fe1f6916ab000b8
Keneth Oseguera <keneth.oseguera@gmail.com>
2020-10-08 00:02:42 -0500

    Realizar estructura de datos de tipo Queue

--- DS/queue.py
+++ DS/queue.py
@@ -1,39 +1,55 @@
 from node import Node
 
-class Stack:
-  def __init__(self, limit=1000):
-    self.top_item = None
+class Queue:
+  def __init__(self, max_size=None):
+    self.head = None
+    self.tail = None
+    self.max_size = max_size
     self.size = 0
-    self.limit = limit
-
-  
-  def push(self, value):
+    
+  def enqueue(self, value):
     if self.has_space():
-      item = Node(value)
-      item.set_next_node(self.top_item)
-      self.top_item = item
+      item_to_add = Node(value)
+      print("Adding " + str(item_to_add.get_value()) + " to the queue!")
+      if self.is_empty():
+        self.head = item_to_add
+        self.tail = item_to_add
+      else:
+        self.tail.set_next_node(item_to_add)
+        self.tail = item_to_add
       self.size += 1
-      print("Adding {} to the pizza stack!".format(value))
     else:
-      print("No room for {}!".format(value))
-
-  def pop(self):
-    if not self.is_empty():
-      item_to_remove = self.top_item
-      self.top_item = item_to_remove.get_next_node()
+      print("Sorry, no more room!")
+         
+  def dequeue(self):
+    if self.get_size() > 0:
+      item_to_remove = self.head
+      print(str(item_to_remove.get_value()) + " is served!")
+      if self.get_size() == 1:
+        self.head = None
+        self.tail = None
+      else:
+        self.head = self.head.get_next_node()
       self.size -= 1
-      print("Delivering " + item_to_remove.get_value())
       return item_to_remove.get_value()
-    print("All out of pizza.")
-
+    else:
+      print("The queue is totally empty!")
+  
   def peek(self):
-    if not self.is_empty():
-      return self.top_item.get_value()
-    print("Nothing to see here!")
-
+    if self.size > 0:
+      return self.head.get_value()
+    else:
+      print("No orders waiting!")
+  
+  def get_size(self):
+    return self.size
+  
   def has_space(self):
-    return self.limit > self.size
-
+    if self.max_size == None:
+      return True
+    else:
+      return self.max_size > self.get_size()
+    
   def is_empty(self):
     return self.size == 0
-  
+
